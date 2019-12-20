7345dcbe0408747e62d1ff3d6e32bbd84959ff5c
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-12-20 08:31:45 +0000

    Handle Discounts section display

--- src/KartUI.js
+++ src/KartUI.js
@@ -13,11 +13,11 @@ const calculateTotalItems = kart => {
     : `${numberOfItems} items`;
 };
 
+const calculateUndiscounted = kart =>
+  kart.reduce((total, { quantity, price }) => total + quantity * price, 0);
+
 const calculateUndiscountedCost = kart => {
-  const undiscounted = kart.reduce(
-    (total, { quantity, price }) => total + quantity * price,
-    0
-  );
+  const undiscounted = calculateUndiscounted(kart);
   return undiscounted > 0 ? `${undiscounted} €` : "";
 };
 
@@ -26,17 +26,27 @@ const offerType = (name, quantity, twoForOne, bulkDiscount) =>
     bulkDiscount ? `x${quantity} ` : ""
   }${name} POP Offer`;
 
-const displayIndivdualDiscounts = kart => {
-  kart.forEach(({ name, price, quantity, twoForOne, bulkDiscount, cost }) => {
-    if ((twoForOne || bulkDiscount) && quantity > 1) {
-      $(`#${name.toLowerCase()}-discount`).html(`
+const displayIndivdualDiscounts = (kart, totalCost) => {
+  const shouldDisplay = calculateUndiscounted(kart) - totalCost > 0;
+
+  if (shouldDisplay) {
+    $("#discout-display").css("visibility", "visible");
+
+    kart.forEach(({ name, price, quantity, twoForOne, bulkDiscount, cost }) => {
+      if ((twoForOne || bulkDiscount) && quantity > 1) {
+        $(`#${name.toLowerCase()}-discount`).css("display", "block").html(`
         <div>
           <div>${offerType(name, quantity, twoForOne, bulkDiscount)}</div>
           <div>${quantity * price - cost}</div>
         </div>
       `);
-    }
-  });
+      } else {
+        $(`#${name.toLowerCase()}-discount`).css("display", "none");
+      }
+    });
+  } else {
+    $("#discout-display").css("visibility", "hidden");
+  }
 };
 
 // DOM Manipulation through jQuery
@@ -46,13 +56,14 @@ $(document).ready(() => {
 
   $("#total-cost").text(0);
   $("#total-items").text("No item");
+  $("#discout-display").css("visibility", "hidden");
 
   const updateShop = (name, shop, itemQuantityId, itemCostId) => {
     $(`#${itemQuantityId}`).text(findItem(name, shop.kart).quantity);
     $(`#${itemCostId}`).text(findItem(name, shop.kart).cost + " €");
     $("#total-items").text(calculateTotalItems(shop.kart));
     $("#undiscounted-cost").text(calculateUndiscountedCost(shop.kart));
-    displayIndivdualDiscounts(shop.kart);
+    displayIndivdualDiscounts(shop.kart, shop.totalCost);
     $("#total-cost").text(shop.totalCost);
   };
 
